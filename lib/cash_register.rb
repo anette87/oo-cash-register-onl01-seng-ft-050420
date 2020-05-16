896f0329d3c4a23d880bea3b2b0b3fbfc1b04f2a
Anette Rivera <anettem.rivera@gmail.com>
2020-05-16 19:35:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,20 +12,20 @@ class CashRegister
     return @total 
   end 
 
-  def add_item(item, price, quantity = 1)
-    @price = price
-    @total += price * quantity
-    @items = []
-      if quantity > 1
-          counter = 0
-      while counter < quantity
-            @items << item
-            counter += 1
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
       end
-      else
-      @items << item
+    else
+      @items << title
     end
-  end 
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
 
   def apply_discount
     if @discount > 0
